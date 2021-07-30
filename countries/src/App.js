250a6f6469ce865cb24ea34c754fd3dc11398438
Alejandro <largoalejo@gmail.com>
2021-07-30 18:15:59 -0500

    2.12 countries

---
+++ countries/src/App.js
@@ -0,0 +1,62 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    if (search === '') return;
+    const url = `https://restcountries.eu/rest/v2/name/${search}`;
+    axios.get(url).then((response) => {
+      setCountries(response.data);
+    });
+  }, [search]);
+
+  const handleCountry = (e) => {
+    setSearch(e.target.value);
+  };
+
+  return (
+    <div>
+      <div>
+        find countries:{' '}
+        <input type='text' value={search} onChange={handleCountry} />
+      </div>
+      <div>
+        {countries.length >= 10 ? (
+          <p>Too many matches, specify another filter</p>
+        ) : countries.length === 1 ? (
+          countries.map(
+            ({ name, numericCode, capital, population, languages, flag }) => {
+              return (
+                <div key={numericCode}>
+                  <h3>{name}</h3>
+                  <p>{capital}</p>
+                  <p>Populatio: {population}</p>
+                  <h4>Languages</h4>
+                  <ul>
+                    {languages.map(({ name }) => {
+                      return <li>{name}</li>;
+                    })}
+                  </ul>
+                  <img
+                    src={flag}
+                    alt={name}
+                    style={{ width: 100, border: '1px solid black' }}
+                  />
+                </div>
+              );
+            }
+          )
+        ) : (
+          countries.map(({ name, numericCode }) => {
+            return <p key={numericCode}>{name}</p>;
+          })
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
