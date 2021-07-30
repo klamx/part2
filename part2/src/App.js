1763c778404adce19ea16b5a80f7eeb51a549af2
Alejandro <largoalejo@gmail.com>
2021-07-30 11:57:53 -0500

    formularios

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,12 +1,50 @@
+import { useState } from 'react';
 import { Note } from './components/Note';
 
-function App({ notes }) {
+function App(props) {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('');
+  const [showAll, setShowAll] = useState(true);
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObj = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+    setNotes([...notes, noteObj]);
+    setNewNote('');
+  };
+
+  const handleNewNote = (event) => {
+    setNewNote(event.target.value);
+  };
+
+  const toggleFilter = () => {
+    setShowAll(!showAll);
+  };
+
   return (
     <div>
       <h1>Notes</h1>
-      {notes.map((note) => {
-        return <Note key={note.id} note={note} />;
-      })}
+      <div>
+        <button onClick={toggleFilter}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => {
+          return <Note key={note.id} note={note} />;
+        })}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNewNote} />
+        <button type='submit'>save</button>
+      </form>
     </div>
   );
 }
