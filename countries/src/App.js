020dbaf54e83dea42b2f0466f5aae9d00ff88547
Alejandro <largoalejo@gmail.com>
2021-07-30 19:34:59 -0500

    2.14 api usage limit reached

--- countries/src/App.js
+++ countries/src/App.js
@@ -4,6 +4,10 @@ import { useEffect, useState } from 'react';
 function App() {
   const [countries, setCountries] = useState([]);
   const [search, setSearch] = useState('');
+  // const [weather, setWeather] = useState({});
+  // const [country, setCountry] = useState({ name: '', capital: '' });
+
+  // const api_key = process.env.REACT_APP_API_KEY;
 
   useEffect(() => {
     if (search === '') return;
@@ -12,6 +16,20 @@ function App() {
       setCountries(response.data);
     });
   }, [search]);
+
+  // useEffect(() => {
+  //   if (countries.length !== 1) return;
+  //   const url2 = `http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}, ${country.name}`;
+  //   axios.get(url2).then((response) => {
+  //     setWeather(response.data);
+  //   });
+  // }, [country, api_key, countries]);
+
+  // useEffect(() => {
+  //   if (countries.length === 1) {
+  //     setCountry(countries[0]);
+  //   }
+  // }, [countries]);
 
   const handleCountry = (e) => {
     setSearch(e.target.value);
@@ -38,33 +56,39 @@ function App() {
         {countries.length >= 10 ? (
           <p>Too many matches, specify another filter</p>
         ) : countries.length === 1 ? (
-          countries.map(
-            ({ name, numericCode, capital, population, languages, flag }) => {
-              return (
-                <div key={numericCode}>
-                  <h3>{name}</h3>
-                  <p>{capital}</p>
-                  <p>Populatio: {population}</p>
-                  <h4>Languages</h4>
-                  <ul>
-                    {languages.map(({ name }) => {
-                      return <li>{name}</li>;
-                    })}
-                  </ul>
-                  <img
-                    src={flag}
-                    alt={name}
-                    style={{ width: 100, border: '1px solid black' }}
-                  />
-                </div>
-              );
-            }
-          )
-        ) : (
-          countries.map(({ name, numericCode }) => {
+          countries.map(({ name, capital, population, languages, flag }) => {
             return (
-              <div style={{ marginTop: '3px' }}>
-                <span key={numericCode}>{name}</span>{' '}
+              <div key={name}>
+                <h3>{name}</h3>
+                <p>{capital}</p>
+                <p>Populatio: {population}</p>
+                <h4>Languages</h4>
+                <ul>
+                  {languages.map(({ name }) => {
+                    return <li>{name}</li>;
+                  })}
+                </ul>
+                <img
+                  src={flag}
+                  alt={name}
+                  style={{ width: 100, border: '1px solid black' }}
+                />
+                {/* {weather !== {} && (
+                  <>
+                    <h4>Weather in {capital}</h4>
+                    <p>
+                      temperature: {weather.current.temperature} Celsius
+                    </p>{' '}
+                  </>
+                )} */}
+              </div>
+            );
+          })
+        ) : (
+          countries.map(({ name }) => {
+            return (
+              <div key={name} style={{ marginTop: '3px' }}>
+                <span>{name}</span>{' '}
                 <button value={name} onClick={handleShow}>
                   show
                 </button>
